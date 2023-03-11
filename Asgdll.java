84f35f847633ef2a45aba3d63a78e3e7816ea97d
RoswindaAnnas <127526123+RoswindaAnnas@users.noreply.github.com>
2023-03-12 00:11:43 +0800

    Add files via upload

---
+++ Asgdll.java
@@ -0,0 +1,24 @@
+/*NIM   : 13020210268
+Nama    : Roswinda.A
+Hari/Tgl: Sabtu, 11 Maret 2023
+Waktu   : 01:10 AM*/
+package tugas1;
+
+public class Asgdll {
+
+    /**
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        float f=20.0f;
+        double fll;
+
+        /* Algoritma */
+        fll= 10.0f;
+        System.out.println("f : " +f+ "\nfll: " +fll);
+    }
+    
+}
---
+++ Asign.java
@@ -0,0 +1,23 @@
+/*NIM   : 13020210268
+Nama    : Roswinda.A
+Hari/Tgl: Sabtu, 11 Maret 2023
+Waktu   : 02:15 AM*/
+package tugas2;
+
+public class Asign {
+
+    /**
+     * @param args
+     */
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* kamus */
+        int i;
+        /* Program */
+        System.out.print("hello\n"); i=5;
+        System.out.println("Ini nilai i : " +i);
+    }
+    
+}
